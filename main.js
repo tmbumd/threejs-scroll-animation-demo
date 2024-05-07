e03c820700a36e9c4b3a28612ed1385083f97667
Timothy Blackwell <timbwell@gmail.com>
2024-05-06 22:14:20 -0400

    na

--- main.js
+++ main.js
@@ -14,7 +14,7 @@ const renderer = new THREE.WebGLRenderer({
 
 renderer.setPixelRatio(window.devicePixelRatio);
 renderer.setSize(window.innerWidth, window.innerHeight);
-camera.position.setZ(30);
+camera.position.setZ(20);
 camera.position.setX(-3);
 
 renderer.render(scene, camera);
