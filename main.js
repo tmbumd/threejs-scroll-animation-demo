bcda3442c384321f1a7e5dac0da796d3f061c8ae
Timothy Blackwell <timbwell@gmail.com>
2024-05-09 22:33:29 -0400

    delete moon and last false instance

--- main.js
+++ main.js
@@ -17,7 +17,7 @@ renderer.setPixelRatio(window.devicePixelRatio);
 renderer.setSize(window.innerWidth, window.innerHeight);
 camera.position.setZ(30);
 camera.position.setX(-3);
-const gltfLoader = new GLTFLoader();
+
 
 renderer.render(scene, camera);
 
